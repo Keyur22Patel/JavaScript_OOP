b8de0dc0743fedd362618c7ca85eb3b3d1312a1a
_Keyur Patel_ <_[keyur22p@gmail.com]_>
2022-06-16 13:15:18 -0500

    Gameing for users

---
+++ index2.js
@@ -0,0 +1,16 @@
+
+
+
+let random =  7;
+console.log(random);
+
+ let gnum = prompt('Guess the number between 1 and 100 inclusive');
+ if (gnum == random){
+   console.log('Matched');
+ }
+  else{
+   console.log('Not matched, the number was '+gnum);
+  }
+  
+
+   
